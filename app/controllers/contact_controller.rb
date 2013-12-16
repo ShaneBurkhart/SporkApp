9c237296881b0a649892f23de8c17a01e2e8f025
Shane Burkhart <shaneburkhart@gmail.com>
2013-12-15 19:29:03 -0600

    Controllers and views

--- app/controllers/contact_controller.rb
+++
@@ -1,9 +0,0 @@
-class ContactController < ApplicationController
-  def index
-  end
-
-  def message
-
-    redirect_to root_path, notice: "Your message has been sent and we will get back with you soon!"
-  end
-end
